1d7b01db98b41adbf1b6b8b1370dfe46e6560b2d
Josh Akeman <joshakeman@gmail.com>
2019-09-08 11:59:52 -0400

    finished predictions, more visuzalizations

--- predict.py
+++ predict.py
@@ -1,3 +1,13 @@
+#More sklearn imports for models
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+
 import pandas as pd
 import datetime
 import pandas_datareader.data as web
@@ -6,6 +16,9 @@ from pandas import Series, DataFrame, plotting
 # %matplotlib inline
 import matplotlib.pyplot as plt
 from matplotlib import style
+
+import math
+import numpy as np
 
 
 start = datetime.datetime(2010, 1, 1)
@@ -64,4 +77,94 @@ plt.imshow(corr, cmap='hot', interpolation='none')
 plt.colorbar()
 plt.xticks(range(len(corr)), corr.columns)
 plt.yticks(range(len(corr)), corr.columns)
+plt.show()
+
+#Average returns (Return Rate) and standard deviation of returns (Risk)
+plt.scatter(retscomp.mean(), retscomp.std())
+plt.xlabel('Expected returns')
+plt.ylabel('Risk')
+for label, x, y in zip(retscomp.columns, retscomp.mean(), retscomp.std()):
+    plt.annotate(
+        label, 
+        xy = (x, y), xytext = (20, -20),
+        textcoords = 'offset points', ha = 'right', va = 'bottom',
+        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+plt.show()
+
+#Engineer features for prediction models. High Low Percentage 
+#and Percentage Change
+
+dfreg = df.loc[:,['Adj Close','Volume']]
+dfreg['HL_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0
+dfreg['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100.0
+
+#Prep data for use in prediction model
+
+# Drop missing value
+dfreg.fillna(value=-99999, inplace=True)
+# We want to separate 1 percent of the data to forecast
+forecast_out = int(math.ceil(0.01 * len(dfreg)))
+# Separating the label here, we want to predict the AdjClose
+forecast_col = 'Adj Close'
+dfreg['label'] = dfreg[forecast_col].shift(-forecast_out)
+X = np.array(dfreg.drop(['label'], 1))
+# Scale the X so that everyone can have the same distribution for linear regression
+X = sklearn.preprocessing.scale(X)
+# Finally We want to find Data Series of late X and early X (train) for model generation and evaluation
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+# Separate label and identify it as y
+y = np.array(dfreg['label'])
+y = y[:-forecast_out]
+
+
+#Simple linear analysis (linear regression) and Quadratic Discriminant
+#Analysis (can draw a curve)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Linear regression
+clfreg = LinearRegression(n_jobs=-1)
+clfreg.fit(X_train, y_train)
+# Quadratic Regression 2
+clfpoly2 = make_pipeline(PolynomialFeatures(2), Ridge())
+clfpoly2.fit(X_train, y_train)
+# Quadratic Regression 3
+clfpoly3 = make_pipeline(PolynomialFeatures(3), Ridge())
+clfpoly3.fit(X_train, y_train)
+
+# K Nearest Neighbors Regression
+clfknn = KNeighborsRegressor(n_neighbors=2)
+clfknn.fit(X_train, y_train)
+
+#Evaluate our models
+confidencereg = clfreg.score(X_test, y_test)
+confidencepoly2 = clfpoly2.score(X_test,y_test)
+confidencepoly3 = clfpoly3.score(X_test,y_test)
+confidenceknn = clfknn.score(X_test, y_test)
+# results
+print('The linear regression confidence is ', 0.96399641826551985)
+print('The quadratic regression 2 confidence is ', 0.96492624557970319)
+print('The quadratic regression 3 confidence is ', 0.9652082834532858)
+print('The knn regression confidence is ', 0.92844658034790639)
+
+#Plot the forecast
+last_date = dfreg.iloc[-1].name
+last_unix = last_date
+next_unix = last_unix + datetime.timedelta(days=1)
+
+#print some stock forecast
+forecast_set = clfreg.predict(X_lately)
+dfreg['Forecast'] = np.nan
+print(forecast_set)
+
+for i in forecast_set:
+    next_date = next_unix
+    next_unix += datetime.timedelta(days=1)
+    dfreg.loc[next_date] = [np.nan for _ in range(len(dfreg.columns)-1)]+[i]
+dfreg['Adj Close'].tail(500).plot()
+dfreg['Forecast'].tail(500).plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
 plt.show()
